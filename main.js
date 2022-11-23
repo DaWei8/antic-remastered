fc7f46db0899b2afd89d58838afe9af75b8c69b3
King David <45265898+DaWei8@users.noreply.github.com>
2022-11-23 21:14:14 +0100

    worked on the hamburger menu, index.html and css files, added the main.js file

---
+++ main.js
@@ -0,0 +1,8 @@
+var hamburger = document.querySelector('.hamburger');
+var navigation = document.querySelector('.hide-nav')
+
+function activate() {
+    hamburger.classList.toggle('show-hamburger');
+    navigation.classList.toggle('show-nav')
+    
+}
