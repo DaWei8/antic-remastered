ca5f05eac0882a4291b2667f8c072a430bdb9b7a
King David <45265898+DaWei8@users.noreply.github.com>
2022-11-24 05:57:02 +0100

    made modification to the styles.css

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
+import Swiper from 'swiper';
+  // import Swiper styles
+import 'swiper/css';
+
 var hamburger = document.querySelector('.hamburger');
 var navigation = document.querySelector('.hide-nav')
+
+
 
 function activate() {
     hamburger.classList.toggle('show-hamburger');
     navigation.classList.toggle('show-nav')
     
 }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
