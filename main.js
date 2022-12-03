1ac6d2b52917ed5d0c301bb640844be51f7fd2a5
King David <45265898+DaWei8@users.noreply.github.com>
2022-12-03 13:08:49 +0100

    updated the mobile.css file

--- main.js
+++ main.js
@@ -1,36 +1,30 @@
-import Swiper from 'swiper';
-  // import Swiper styles
-import 'swiper/css';
+// import Swiper from 'swiper';
+// import Swiper styles
+// import 'swiper/css';
 
 var hamburger = document.querySelector('.hamburger');
-var navigation = document.querySelector('.hide-nav')
+var navigation = document.querySelector('.hide-nav');
 
+var swiper = new Swiper(".carousel-content", {
+  
+  slidesPerView: 3,
+  spaceBetween: 1,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".move-left i",
+    prevEl: ".move-right i",
+  },
+});
 
 
 function activate() {
+    navigation.classList.toggle('show-nav');
     hamburger.classList.toggle('show-hamburger');
-    navigation.classList.toggle('show-nav')
     
 }
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
